d8a63f0cf50140e9d8c78b31554c84bb6e3ca155
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-05-18 16:39:22 +0300

    Updated 4 google

--- index.php
+++ index.php
@@ -6,23 +6,15 @@ class LinkParser
 	/**
 	 * @var string  what we parce
 	 */
-	public $parcing_site = 'https://www.ua-region.info';
-	/**
-	 * @var string url where are categories
-	 */
-	public $first_url = 'https://www.ua-region.info/kved/';
+	public $url = 'https://www.google.com.ua/search?q=site:.org.ua&num=10';
 	/**
 	 * @var string Additional param 4 pager, recurse get pages
 	 */
-	public $pager_param = '?start_page=';
+	private $search_mask = "/https?:\/\/(?:www\.)?[-0-9a-zа-яA-ZА-Я\.]+.org.ua/i";
 	/**
-	 * @var array of catalogs (kveds)
+	 * @var string param which uses 4 step 2 another page
 	 */
-	public $array_of_kveds = array();
-	/**
-	 * @var array resulting array of companies
-	 */
-	public $array_of_companies = array();
+	public $pager_param = '&start=';
 	/**
 	 * @var array of getting links
 	 */
@@ -32,10 +24,6 @@ class LinkParser
 	 */
 	private $download_folder = 'download/';
 	/**
-	 * @var string file. where sore kveds
-	 */
-	private $file_kveds = "kveds.json";
-	/**
 	 * @var string  where storing data with URLs and emails
 	 */
 	private $file_urls = "urls.txt";
@@ -44,16 +32,15 @@ class LinkParser
 	 */
 	private $file_logs = "log.txt";
 	/**
-	 * @var int time before next page
+	 * @var int count per page
 	 */
-	// private $sleep_time;
+	private $start_index;
 
-//// print_r($html);
-
-	public function __construct()
+	public function __construct($start_index)
 	{
 		include_once('simplehtmldom_1_5/simple_html_dom.php');
 		include_once('getcurl.php');
+		$this->start_index = $start_index;
 	}
 
 	/**
@@ -63,7 +50,6 @@ class LinkParser
 	 */
 	function getHtml($url)
 	{
-//        echo '<p>Getting HTML...</p>';
 		$html = curl_get($url);
 		return $html;
 	}
@@ -75,7 +61,6 @@ class LinkParser
 	 */
 	function getDom($html)
 	{
-//        echo '<p>Getting DOM...</p>';
 		$dom = str_get_html($html);
 		return $dom;
 	}
@@ -87,58 +72,12 @@ class LinkParser
 	 */
 	function getLinks($dom)
 	{
-//        echo '<p>Searching 4 links...</p>';
 		$array_of_links = array();
 		foreach ($dom->find('a') as $e) {
-			$href_url = $e->href;
-			$array_of_links[] = $href_url;
-//            echo $href_url . '<br>';
+			$array_of_links[] = strtolower($e->href);
 ///  if(!file_exists('download/'.$imagename)){copy($imgurl,"download/$imagename");}
 		}
 		return $array_of_links;
-	}
-
-	/**
-	 * get H1 caption of category
-	 * @param $dom
-	 * @return string
-	 */
-	/*private function getH1ofCompanies($dom)
-	{
-//        echo '<p>Get H1 of category...</p>';
-		$h1 = $dom->find('h1.b-company-rating-header');
-		return $h1[0];
-	}*/
-
-	/**
-	 * get H1 caption of category
-	 * @param $dom
-	 * @return Integer
-	 */
-	private function getH2ofCompanies($dom)
-	{
-//        echo '<p>Searching names of companies count...</p>';
-		$h2 = $dom->find('h2[itemprop="name"]');
-		return count($h2);
-	}
-
-	/**
-	 * put only links 4 kved
-	 * @param Array $array_of_links
-	 * @return array
-	 */
-	function sortKveds($array_of_links)
-	{
-//        echo '<p>Sorting Categories (kveds)...</p>';
-		$kveds = array();
-		$array_of_kveds = preg_grep("/^[\/]kved\/[\w\d]+\./", $array_of_links);
-		foreach ($array_of_kveds as $kved) {
-			$kveds[] = $kved;
-		}
-		if (!file_put_contents($this->download_folder . $this->file_kveds, json_encode($kveds))) {
-			file_put_contents($this->download_folder . $this->file_logs, 'kved not wrote.' . PHP_EOL, FILE_APPEND | LOCK_EX);
-		};
-		return $kveds;
 	}
 
 	/**
@@ -146,13 +85,10 @@ class LinkParser
 	 * @param $array_of_links
 	 * @return array
 	 */
-	function sortEmailsAndUrl($array_of_links)
+	function sortUrl($array_of_links)
 	{
-//        $kved_file_urls = ($kved_file_urls) ? preg_replace("/[\/.-:](?:[a-zA-Z]+)[\/.-:]([a-zA-z]+)/", "$1", $kved_file_urls) : $this->file_urls;
-//        echo '<p>Getting emails and URLs...</p>';
-//        $array_of_url = preg_grep("/(https?:\/\/[^plus][\w]+\.[^ua|fac]+)|(mailto:(?!info))|(^[/][/\d]+\z)/", $array_of_links);
-		$array_of_url = preg_grep("/(https?:\/\/(?!(?:www.)?(?:ua-region[\w.]+|facebook|twitter)|vk|plus.google))|(mailto:(?!(?:info|admin)@ua-region))|(^[\/][\/\d]+\z)/", $array_of_links);
-		if (!file_put_contents($this->download_folder . "$this->file_urls", implode(PHP_EOL, $array_of_url), FILE_APPEND | LOCK_EX)) {
+		preg_match_all($this->search_mask, $array_of_links, $array_of_url);
+		if (!file_put_contents($this->download_folder . "$this->file_urls", implode(PHP_EOL, $array_of_url[0]), FILE_APPEND | LOCK_EX)) {
 			file_put_contents($this->download_folder . $this->file_logs, 'url not wrote. ' . PHP_EOL, FILE_APPEND | LOCK_EX);
 		};
 		return $array_of_url;
@@ -162,125 +98,50 @@ class LinkParser
 // INIT
 	function init($url)
 	{
-//        echo '<p>We are started...</p>';
 		$html = $this->getHtml($url);
 		$dom = $this->getDom($html);
 		$array_of_links = $this->getLinks($dom);
-		$array_of_kveds = $this->sortKveds($array_of_links);
-		return $array_of_kveds;
-//        $this->recurseKvedArray($array_of_kveds);
+		# TODO: debug section
+//		$array_of_links = "/search?q=site:.org.ua&amp;gbv=1&amp;sei=1MD-WueaAaTt6QSC-ZA4https://www.google.com.ua/intl/uk/options/https://accounts.google.com/ServiceLogin?hl=uk&amp;passive=true&amp;continue=https://www.google.com.ua/search%3Fq%3Dsite:.org.ua%26num%3D10https://www.google.com.ua/webhp?hl=uk&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQPAgD//support.google.com/websearch/answer/186645?hl=uk#/search?q=site:.org.ua&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUICigB/search?q=site:.org.ua&amp;source=lnms&amp;tbm=nws&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUICygChttps://maps.google.com.ua/maps?q=site:.org.ua&amp;num=10&amp;um=1&amp;ie=UTF-8&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUIDCgD/search?q=site:.org.ua&amp;source=lnms&amp;tbm=vid&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUIDigA/search?q=site:.org.ua&amp;source=lnms&amp;tbm=bks&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUIDygBhttps://www.google.com.ua/flights?q=site:.org.ua&amp;source=lnms&amp;tbm=flm&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUIECgC/search?q=site:.org.ua&amp;source=lnms&amp;tbm=fin&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQ_AUIESgD/preferences/preferences?hl=uk&amp;prev=https://www.google.com.ua/search?q%3Dsite:.org.ua%26num%3D10/preferences?hl=uk&amp;prev=https://www.google.com.ua/search?q%3Dsite:.org.ua%26num%3D10#languages/setprefs?safeui=on&amp;sig=0_1M75zo3HQI8olpKZaWPN34obdgY%3D&amp;prev=https://www.google.com.ua/search?q%3Dsite:.org.ua%26num%3D10/advanced_search?q=site:.org.ua&amp;hl=uk/history/optout?hl=uk//support.google.com/websearch/?source=g&amp;hl=ukhttps://www.google.com/webmasters/tools/home#utm_source=uk-wmxmsg&amp;utm_medium=wmxmsg&amp;utm_campaign=bm&amp;authuser=0https://pasport.org.ua/#https://webcache.googleusercontent.com/search?q=cache:rIyf7Ci5ekoJ:https://pasport.org.ua/+&amp;cd=1&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:https://pasport.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwgqMAAhttp://x1.org.ua/#http://webcache.googleusercontent.com/search?q=cache:Ij8aXEsCW7MJ:x1.org.ua/+&amp;cd=2&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:x1.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwgwMAEhttps://translate.google.com.ua/translate?hl=uk&amp;sl=ru&amp;u=http://x1.org.ua/&amp;prev=searchhttps://toursector.org.ua/#https://webcache.googleusercontent.com/search?q=cache:vEHXYDiSr_QJ:https://toursector.org.ua/+&amp;cd=3&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:https://toursector.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwg4MAIhttps://translate.google.com.ua/translate?hl=uk&amp;sl=ru&amp;u=https://toursector.org.ua/&amp;prev=searchhttps://ba.org.ua/#https://webcache.googleusercontent.com/search?q=cache:HCdT2cHu-x8J:https://ba.org.ua/+&amp;cd=4&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:https://ba.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwhAMAMhttps://www.schoollife.org.ua/#https://webcache.googleusercontent.com/search?q=cache:Av9uELBX1mwJ:https://www.schoollife.org.ua/+&amp;cd=5&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:https://www.schoollife.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwhGMAQhttp://nus.org.ua/#http://webcache.googleusercontent.com/search?q=cache:r9nOpET38ToJ:nus.org.ua/+&amp;cd=6&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:nus.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwhMMAUhttp://naiu.org.ua/#http://webcache.googleusercontent.com/search?q=cache:_6CbCiGViucJ:naiu.org.ua/+&amp;cd=7&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:naiu.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwhSMAYhttp://www.novilidery.org.ua/#http://webcache.googleusercontent.com/search?q=cache:zLEzxejvohUJ:www.novilidery.org.ua/+&amp;cd=8&amp;hl=uk&amp;ct=clnk&amp;gl=uahttp://speedtest.org.ua/#http://webcache.googleusercontent.com/search?q=cache:lj4SvjYN6UMJ:speedtest.org.ua/+&amp;cd=9&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=related:speedtest.org.ua/&amp;tbo=1&amp;sa=X&amp;ved=0ahUKEwjn1MzOm4_bAhWkdpoKHYI8BAcQHwhdMAghttps://translate.google.com.ua/translate?hl=uk&amp;sl=ru&amp;u=http://speedtest.org.ua/&amp;prev=searchhttp://tau.org.ua/#http://webcache.googleusercontent.com/search?q=cache:SEKPZZhNUlUJ:tau.org.ua/+&amp;cd=10&amp;hl=uk&amp;ct=clnk&amp;gl=ua/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=10&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=20&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=30&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=40&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=50&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=60&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=70&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=80&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=90&amp;sa=N/search?q=site:.org.ua&amp;ei=1MD-WueaAaTt6QSC-ZA4&amp;start=10&amp;sa=N#https://support.google.com/websearch?p=ws_settings_location&amp;hl=uk//support.google.com/websearch/?p=ws_results_help&amp;hl=uk&amp;fg=1#//www.google.com.ua/intl/uk_ua/policies/privacy/?fg=1//www.google.com.ua/intl/uk_ua/policies/terms/?fg=1https://myaccount.google.com/?utm_source=OGB&amp;utm_medium=apphttps://www.google.com.ua/webhp?tab=wwhttps://maps.google.com.ua/maps?hl=uk&amp;tab=wlhttps://www.youtube.com/?gl=UAhttps://news.google.com.ua/nwshp?hl=uk&amp;tab=wnhttps://mail.google.com/mail/?tab=wmhttps://drive.google.com/?tab=wohttps://www.google.com/calendar?tab=wchttps://plus.google.com/?gpsrc=ogpy0&amp;tab=wXhttps://translate.google.com.ua/?hl=uk&amp;tab=wThttps://photos.google.com/?tab=wq&amp;pageId=nonehttps://www.google.com.ua/intl/uk/options/https://docs.google.com/document/?usp=docs_alchttps://www.blogger.com/?tab=wjhttps://www.google.com/contacts/?hl=uk&amp;tab=wChttps://hangouts.google.com/https://keep.google.com/https://www.google.com.ua/intl/uk/options/https://DDD.org.ua";
+		$sorted_links = $this->sortUrl(implode($array_of_links));
+		# TODO: debug section
+//		$sorted_links = $this->sortUrl($array_of_links);
+		var_dump($sorted_links);
+		$this->nextPage();
 	}
 
 	/**
-	 * recursing in pages in kved category
-	 * @param $kved
+	 * recursing in pages
+	 *
 	 */
-	function recurseKvedArray($kved)
+	function nextPage()
 	{
-//        echo '<p>Kveds recurse started...</p>';
-//            $kved = reset($array_of_kveds);
-		for ($i = 3; ; $i++) {
-			$url = $this->parcing_site . $kved . $this->pager_param . $i;
+		$has_links = TRUE;
+		$start_index = $this->start_index;
+		while ($has_links) {
+			$sleep_time = rand(80, 122);
+			echo "<p>Sleep $sleep_time - " . date('F-d-H:i:s') . "</p>";
+			sleep($sleep_time);
+			$start_index += 100;
+			$url = $this->url . $this->pager_param . $start_index;
+			# TODO: debug section
+//			$url = $this->url . $this->pager_param . 500;
 			echo "<p><i>$url</i></p>";
 			$html = $this->getHtml($url);
 			$dom = $this->getDom($html);
-			if (!empty($dom)) {
-				$arr_of_companies = $this->getLinks($dom);
-				$h2 = $this->getH2ofCompanies($dom);
-				$links = $this->sortEmailsAndUrl($arr_of_companies);
-				var_dump($links);
-				$sleep_time = rand(80, 122);
-				echo "<p>Sleep $sleep_time - " . date('F-d-H:i:s') . "</p>";
-				sleep($sleep_time);
-				if ($h2 == 0) {
-					echo "<p>Count of h2 is 0</p>";
-					break;
-				}
-			} else {
-				file_put_contents($this->download_folder . $this->file_logs, "<p>$kved Array is empty</p>", FILE_APPEND | LOCK_EX);
-				continue;
-			}
+			$array_of_links = $this->getLinks($dom);
+			$sorted_links = $this->sortUrl(implode($array_of_links));
+			var_dump($sorted_links);
+			$has_links = count($sorted_links[0]);
 		}
-	}
-
-	/**
-	 * does kved file exist's
-	 * @return bool
-	 */
-	public function existingKvedFile()
-	{
-		if (file_exists($this->download_folder . $this->file_kveds)) {
-			return True;
-		} else {
-			echo "<p>File Kveds <b>doesn't exists</b></p>";
-			return False;
-		}
-	}
-
-	/**
-	 * getting Kved file if exist
-	 * @return array|mixed
-	 */
-	public function getKvedFile()
-	{
-		$kveds = array();
-		if ($this->existingKvedFile()) {
-			try {
-				$kveds = json_decode(file_get_contents('download/kveds.json'), true);
-				print_r($kveds);
-			} catch (Exception $e) {
-				echo "<p>$e</p>";
-			}
-		}
-		return $kveds;
+		die('Nothing do here');
 	}
 }
 
 // LET'S FUNNY
 
-
 $start_index = isset($argv[1]) ? $argv[1] : $_GET['start_index'];
-$end_index = isset($argv[2]) ? $argv[2] : $_GET['end_index'];
-$LP = new LinkParser();
-
-// file with kveds exists
-
-if ($start_index and $LP->existingKvedFile()) {
-	$kveds = $LP->getKvedFile();
-	if (count($kveds) == 0) {
-		die('<p>Error with Kved file</p>');
-	}
-	if (key_exists($start_index, $kveds)) {
-		if ($end_index AND key_exists($end_index, $kveds)) {
-			echo "<hr><h3><small>" . date('F-d-H:i') . "</small> Parse from $kveds[$start_index] to $kveds[$end_index]</h3>";
-			for ($i = $start_index; $i <= $end_index; $i++) {
-				$LP->recurseKvedArray($kveds[$i]);
-				$sleep_time = rand(90, 130);
-				sleep($sleep_time);
-				echo "<p>$kveds[$i] <b>done</b>. Sleeped $sleep_time - " . date('F-d-H:i:s') . "&nbsp;<em>$i from $end_index</em></p>";
-			}
-		} else {
-			echo "<h3>Parse from $kveds[$start_index]</h3>";
-			$LP->recurseKvedArray($kveds[$start_index]);
-			echo "<p>$kveds[$start_index] <b>done</b></p>";
-		}
-		echo "<p><b>Well done</b></p>";
-	} else {
-		die("<p><b>$start_index</b> doesn't exist</p>");
-	}
-
-
-} else //new request
-{
-	$arr_of_kveds = $LP->init($LP->first_url);
-	if (count($arr_of_kveds) > 0) {
-		foreach ($arr_of_kveds as $kved) {
-			$LP->recurseKvedArray($kved);
-			$sleep_time = rand(90, 130);
-			sleep($sleep_time);
-			echo "<p>$kved <b>done</b>. Sleep $sleep_time - " . date('F-d-H:i:s') . "</p>";
-		}
-		echo '<p>Kveds stored. Add <pre>?start_index=7007</pre> to display results</p>';
-	} else echo '<p>Nothing do here</p>';
-}
+//$end_index = isset($argv[2]) ? $argv[2] : $_GET['end_index'];
+$LP = new LinkParser($start_index = 0);
+$LP->init($LP->url);
 
