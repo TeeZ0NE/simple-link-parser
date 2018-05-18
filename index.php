c285eeb0ba64aefbc45d7a178939630a42b6421a
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-05-18 17:12:30 +0300

    parcing vn.ua

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class LinkParser
 	/**
 	 * @var string  what we parce
 	 */
-	public $url = 'https://www.google.com.ua/search?q=site:.org.ua&num=100';
+	public $url = 'https://www.google.com.ua/search?q=site:.vn.ua&num=100';
 	/**
 	 * @var string Additional param 4 pager, recurse get pages
 	 */
